77c1b690ce4fb121d9f03bb32077dadf543fab15
giuliano <giuliano@mail.giulianoscaglioni.com>
2018-04-28 19:41:02 -0300

    Work on UserDao
    App can't inject UserJDBCDao on UsersImpl because it says that it can't find it.

--- services/src/main/java/ar/edu/itba/paw/services/UsersImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UsersImpl.java
@@ -1,15 +1,18 @@
 package ar.edu.itba.paw.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import ar.edu.itba.paw.interfaces.UserDao;
 import ar.edu.itba.paw.interfaces.Users;
 import ar.edu.itba.paw.model.User;
 
 @Primary
 @Service
 public class UsersImpl implements Users {
-
+	@Autowired
+	private UserDao userDao;
 	@Override
 	public User findById(Integer id) {
 		if(id == null) {
