b6bd53f80216299cf1a7ecaf080c58c443ed1c59
Daniella Liberman <daliberman@itba.edu.ar>
2018-04-30 01:35:36 -0300

    tutorial 4 almost done!

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,55 +1,45 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.itba.paw.interfaces.Users;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.webapp.form.UserForm;
 
 @Controller
 public class HelloWorldController {
 	
 	@Autowired
 	private Users us;
-	
-	@RequestMapping("/")
-	public ModelAndView helloWorld() {
-		final ModelAndView mav = new ModelAndView("index");
-		mav.addObject("greeting", us.findById(1).getUsername());
-		return mav;
-	}
-	
-	@RequestMapping("/create")
-	public ModelAndView create(
-								@RequestParam(value="username", required = true) final String username,
-								@RequestParam(value="email", required = true) final String email,
-								@RequestParam(value="password", required = true) final String password
-							  ) {
-		final User u = us.create(username, email, password);
-		
-		return new ModelAndView("redirect:/user/" + u.getId());
-	}
-	
-	// Request example: http://localhost:8080/?id=9
-	//
-	//@RequestMapping("/")
-	//public ModelAndView helloWorld(@RequestParam("id") int id) {
-	//	final ModelAndView mav = new ModelAndView("index");
-	//	//mav.addObject("greeting", us.findById(1).getUsername());
-	//	mav.addObject("greeting", us.findById(1).getUsername() + " " + id);
-	//	return mav;
-	//}
-	
 	 
 	@RequestMapping("/user/{id}")
 	public ModelAndView helloWorld(@PathVariable("id") int id) {
-		final ModelAndView mav = new ModelAndView("index");
-		mav.addObject("greeting", us.findById(id).getUsername() + " " + id);
+		final ModelAndView mav = new ModelAndView("user");
+		mav.addObject("user", us.findById(id));
 		return mav;
 	}
 	
+	@RequestMapping("/")
+	public ModelAndView index(@ModelAttribute("registerForm") final UserForm form) {
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping(value = "/create", method = { RequestMethod.POST })
+	public ModelAndView create(@Valid @ModelAttribute("registerForm") final UserForm form, final BindingResult errors) {
+		if (errors.hasErrors()) {
+			return index(form);
+		}
+		final User u = us.create(form.getUsername(), form.getEmail(), form.getPassword());
+		return new ModelAndView("redirect:/user/"+ u.getId());
+	}
 }
