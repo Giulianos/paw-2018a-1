bd5f547705afcec9774b160ffe7c9994db22fea0
Daniella Liberman <daliberman@itba.edu.ar>
2018-04-30 02:57:56 -0300

    first try

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/PawUserDetailsService.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PawUserDetailsService implements UserDetailsService {
+	@Autowired
+	private UserService us;
+	
+	@Override
+	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+		final User user = us.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("No user by the name " + username);
+		}
+		final Collection<? extends GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN"));
+		return new org.springframework.security.core.userdetails.User(username, user.getPassword(), authorities);
+	}
+}
