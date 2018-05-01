888edc52c95836da52066a99f84863a90c7ddb71
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2018-04-30 23:56:44 -0300

    some, fixes (untested)

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.itba.paw.interfaces.Users;
@@ -46,13 +45,5 @@ public class HelloWorldController {
 	@RequestMapping("/login")
 	public ModelAndView login() {
 		return new ModelAndView("login");
-	}
-	
-	@RequestMapping("/admin")
-	@Controller
-	public class AdminController {@RequestMapping("/")
-		public ModelAndView index() {
-			return new ModelAndView("admin/index");
-		}
 	}
 }
