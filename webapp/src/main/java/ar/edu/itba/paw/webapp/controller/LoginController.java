a592cb9d41068971927e693b450c87614ee5323f
Marco Fallone <mfallone@itba.edu.ar>
2018-06-01 02:07:52 -0300

    Validation added to login.
    Login/register button not showing in login page after failed login.

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/LoginController.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.itba.paw.interfaces.Users;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private Users us;
+	
+	@RequestMapping("/login")
+	public ModelAndView login(HttpServletRequest request, ModelMap model) {
+		if (!us.findByUsername(request.getParameter("j_username")).isPresent()) {
+			model.addAttribute("invalid_loginUser", true);
+		} else { // Si el usuario es correcto entonces la contraseña es incorrecta.
+			model.addAttribute("invalid_loginPassword", true);
+		}
+		model.addAttribute("failed_username", request.getParameter("j_username"));
+		return new ModelAndView("login");
+	}
+}
