db3ff39759662ad319d303aa98b035e82332cd3b
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-05-23 00:34:04 -0300

    Add executor service to send emails async

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
@@ -1,31 +1,11 @@
 package ar.edu.itba.paw.interfaces.service;
 
-import ar.edu.itba.paw.model.Publication;
 import ar.edu.itba.paw.model.User;
 
 public interface EmailService {
-	
-	/**
-	 * Sends an email
-	 * 
-	 * @param to The recipient of the email
-	 * @param subject The subject of the email
-	 * @param text The email's content
-	 */
-	
-	public void sendEmail(final String to, final String subject, final String text); 
-	
-	
 	/**
 	 * Send welcome email to user
 	 * @param user User to welcome
 	 */
 	public void welcomeUser(User user);
-	
-	
-	/**
-	 * Send email notifying the fulfillment of a publication
-	 * @param pub Publication that has been fulfilled
-	 */
-	public void notifyPublicationFulfillment(Publication pub);
 }
--- services/src/main/java/ar/edu/itba/paw/services/EmailsImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EmailsImpl.java
@@ -9,75 +9,57 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import ar.edu.itba.paw.interfaces.service.EmailService;
-import ar.edu.itba.paw.model.Order;
-import ar.edu.itba.paw.model.Publication;
 import ar.edu.itba.paw.model.User;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 @Component
-public class EmailsImpl implements EmailService{
+public class EmailsImpl implements EmailService {
 
-	@Autowired
-	private JavaMailSender emailSender;
-	
-	@Override
-	@Async
-	public void sendEmail(String to, String subject, String text) {
-		MimeMessage message = emailSender.createMimeMessage();
-		MimeMessageHelper helper;
-		try {
-			helper = new MimeMessageHelper(message, false, "utf-8");
-			message.setContent(text, "text/html");
-			helper.setTo(to);
-			helper.setSubject(subject);
-			helper.setFrom("gumpuonline@gmail.com");
-			emailSender.send(message);
-		} catch (Exception e) {
-		}
-	}
+    private final int THREADS_QUANTITY = 10;
 
-	@Override
-	public void notifyPublicationFulfillment(Publication pub) {
-		String publicationDescription = pub.getDescription();
-		for(Order order : pub.getOrders()) {
-			if(!order.getSubscriber().getUsername().equals(order.getPublication().getSupervisor().getUsername())) {
-				sendEmail(order.getSubscriber().getEmail(),
-							"[GUMPU] One of your orders needs attention",
-							"<h1>News from GUMPU!</h1>" +
-							"<p>Your order of "+order.getQuantity()+" "+publicationDescription+" needs attention!</p>" +
-							"<p>Contact the supervisor of the publication " +
-								"<a href='" + getAbsoluteURL("profile/messaging/"+pub.getId()) + "'>clicking here!</a></p>"
-							);
-			} else {
-				sendEmail(order.getSubscriber().getEmail(),
-						"[GUMPU] One of your publications needs attention",
-						"<h1>News from GUMPU!</h1>" +
-						"<p>Your publication "+publicationDescription+" needs attention!</p>" +
-						"<p>Contact the subscribers " +
-							"<a href='" + getAbsoluteURL("profile/subscriptions-finalized") + "'>clicking here!</a></p>"
-						);
-			}
-		}
-	}
-	
-	private String getAbsoluteURL(String path) {
-		return "http://pawserver.it.itba.edu.ar/paw-2018a-1/"+path;
-	}
+    @Autowired
+    private JavaMailSender emailSender;
 
-	@Override
-	public void welcomeUser(User user) {
-		sendEmail(user.getEmail(),
-					"[GUMPU] Thanks for signing in!",
-					"<h1>Hi " + user.getUsername() + "!</h1><h3>GUMPU Thanks you for signing in!</h3>" +
-					"<p>You might start...</p>" +
-					"<ul>" +
-						"<li>" +
-							"<a href='" + getAbsoluteURL("profile/publications?newModal=true") + "'>Creating publications</a>" +
-						"</li>" +
-						"<li>" +
-							"<a href='" + getAbsoluteURL("search") + "'>Subscribing to existing publications</a>" +
-						"</li>" +
-					"</ul>"
-					);
-	}
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(THREADS_QUANTITY);
+
+    @Async
+    public void sendEmail(String to, String subject, String text) {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(message, false, "utf-8");
+            message.setContent(text, "text/html");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setFrom("Gumpu <gumpuonline@gmail.com>");
+            emailSender.send(message);
+        } catch (Exception e) {
+        }
+    }
+
+    private String getAbsoluteURL(String path) {
+        return "http://pawserver.it.itba.edu.ar/paw-2018a-1/" + path;
+    }
+
+    @Override
+    public void welcomeUser(User user) {
+        executorService.submit(() -> {
+            sendEmail(user.getEmail(),
+                    "\uD83C\uDF89 Thanks for signing in!",
+                    "<h1>Hi " + user.getName() + "!</h1><h3>GUMPU Thanks you for signing in!</h3>" +
+                            "<p>You might start...</p>" +
+                            "<ul>" +
+                            "<li>" +
+                            "<a href='" + getAbsoluteURL("profile/publications?newModal=true") + "'>Creating publications</a>" +
+                            "</li>" +
+                            "<li>" +
+                            "<a href='" + getAbsoluteURL("search") + "'>Subscribing to existing publications</a>" +
+                            "</li>" +
+                            "</ul>"
+            );
+        });
+    }
 
 }
