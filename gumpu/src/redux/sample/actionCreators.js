60768b774e14171a7c56fefaa73255cdf56a4e84
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-07 18:54:33 -0300

    Add apisauce

--- gumpu/src/redux/sample/actionCreators.js
+++ gumpu/src/redux/sample/actionCreators.js
@@ -1,7 +1,9 @@
 import actions from "./actions";
 import history from 'router/history';
+import sampleService from "services/sample";
 
 export const createSample = () => dispatch => {
+  sampleService.get();
   setTimeout(() => {
     history.push('/');
     dispatch({
