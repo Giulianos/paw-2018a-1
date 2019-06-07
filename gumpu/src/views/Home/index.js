902f94ec2b3d160bf29c5fce5e871498d4e75628
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-07 19:36:35 -0300

    Add CardContainer component

--- gumpu/src/views/Home/index.js
+++ gumpu/src/views/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import CardContainer from 'components/ui/CardContainer';
+
+import styles from './styles.module.scss';
+
 function Home() {
   return (
     <div>
       <h1>This is the Home screen.</h1>
+      <CardContainer className={styles.customCard}>This is the card container</CardContainer>
       <Link to="/secondary">Go to secondary...</Link>
     </div>
   );
