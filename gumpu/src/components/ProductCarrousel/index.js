d00e0c7936f39800cdb0c1a2875d6b044b9af7b6
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-08 05:15:44 -0300

    Add product card layout

--- gumpu/src/components/ProductCarrousel/index.js
+++ gumpu/src/components/ProductCarrousel/index.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 
 import ProductCard from 'components/ui/ProductCard';
 
+import productSummaryMock from 'mocks/productSummary';
+
 function ProductCarrousel({ className }) {
   return (
     <section className="column">
       <h2 className="txt-large mb-24 txt-gray3">Popular products</h2>
       <div className={`${className} row space-between`}>
-        <ProductCard className="mr-32" />
-        <ProductCard className="mr-32" />
-        <ProductCard className="mr-32" />
-        <ProductCard />
+        <ProductCard product={productSummaryMock} className="mr-32" />
+        <ProductCard product={productSummaryMock} className="mr-32" />
+        <ProductCard product={productSummaryMock} className="mr-32" />
+        <ProductCard product={productSummaryMock} />
       </div>
     </section>
   );
--- gumpu/src/components/ui/ProductCard/index.js
+++ gumpu/src/components/ui/ProductCard/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import CardContainer from 'components/ui/CardContainer';
+import RatingIndicator from 'components/ui/RatingIndicator';
 
 import styles from './styles.module.scss';
 
@@ -9,7 +10,17 @@ function ProductCard({ product, className }) {
   return (
     <CardContainer className={`${styles.productCard} ${className} column center-alt pt-16 pb-16 pl-24 pr-24`}>
       { product ? (
-        <div>The product</div>
+          <>
+            <img className={`${styles.image} mb-24`} src={ product.image_url } alt={product.tile} />
+            <h3 className="w100 txt-medium mb-24">{ product.description }</h3>
+            <span className="w100 txt-medium32 txt-gray3 mb-24">$ { product.price }</span>
+            <div className="row">
+              <span className="indicatorDescriptor txt-small txt-gray2">
+                Calificación del organizador
+              </span>
+              <RatingIndicator className="pt-8" value={ product.supervisor_rating } />
+            </div>
+          </>
         ) : (
           <>
             <div className={`${styles.imagePlaceHolder} mb-24`} />
