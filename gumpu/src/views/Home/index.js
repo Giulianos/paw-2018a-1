89852f6f8cb98f9ca2fce541f6533801bb5cf006
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-07 23:16:04 -0300

    Add spacings

--- gumpu/src/views/Home/index.js
+++ gumpu/src/views/Home/index.js
@@ -15,24 +15,28 @@ function Home() {
   return (
     <div>
       <h1>This is the Home screen.</h1>
-      <CardContainer className={styles.customCard}>This is the card container</CardContainer>
-      <Button>PRIMARY</Button>
-      <Button color="red">PRIMARY</Button>
-      <Button color="yellow">PRIMARY</Button>
-      <Button color="green">PRIMARY</Button>
-      <br/>
-      <br/>
-      <Button variant="secondary">SECONDARY</Button>
-      <Button variant="secondary" color="red">SECONDARY</Button>
-      <Button variant="secondary" color="yellow">SECONDARY</Button>
-      <Button variant="secondary" color="green">SECONDARY</Button>
-      <br />
-      <br />
-      <Input value={name} onChange={handleChangeName} />
-      <Input value={name} onChange={handleChangeName} variant="invalid" />
-      <Input value={name} onChange={handleChangeName} variant="valid" />
-      <br />
-      <br />
+      <CardContainer className={styles.customCard}>
+        <h2 className="txt-xlarge mb-48">Inputs</h2>
+        <label className="txt-input-label">Name</label>
+        <Input className="mb-24" value={name} onChange={handleChangeName} />
+        <label className="txt-input-label">Surname</label>
+        <Input className="mb-24" variant="invalid" value={name} onChange={handleChangeName} />
+        <label className="txt-input-label">Email</label>
+        <Input className="mb-24" variant="valid" value={name} onChange={handleChangeName} />
+      </CardContainer>
+
+      <CardContainer className={styles.customCard}>
+        <h2 className="txt-xlarge mb-48">Buttons</h2>
+        <Button className="mb-8">PRIMARY</Button>
+        <Button className="mb-8" color="red">PRIMARY</Button>
+        <Button className="mb-8" color="yellow">PRIMARY</Button>
+        <Button className="mb-16" color="green">PRIMARY</Button>
+
+        <Button className="mb-8" variant="secondary">SECONDARY</Button>
+        <Button className="mb-8" variant="secondary" color="red">SECONDARY</Button>
+        <Button className="mb-8" variant="secondary" color="yellow">SECONDARY</Button>
+        <Button className="mb-8" variant="secondary" color="green">SECONDARY</Button>
+      </CardContainer>
       <Link to="/secondary">Go to secondary...</Link>
     </div>
   );
