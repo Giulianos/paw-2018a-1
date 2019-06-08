ee445b13cacaef499875a7fc99c4e07464f8549c
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-08 01:20:13 -0300

    Add header

--- gumpu/src/components/App/index.js
+++ gumpu/src/components/App/index.js
@@ -3,12 +3,15 @@ import { Router, Route, Switch } from 'react-router-dom';
 
 import history from 'router/history';
 
+import Header from 'components/Header';
+
 import Home from 'views/Home';
 import Secondary from 'views/Secondary';
 
 function App() {
   return (
     <Router history={history}>
+      <Header />
       <Switch>
         <Route path="/secondary" component={Secondary} />
         <Route path="/" component={Home} />
