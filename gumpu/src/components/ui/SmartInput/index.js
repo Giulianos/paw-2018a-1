5b26fc274fe96cb271033f0d68d54203b798e90c
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-09 19:46:15 -0300

    Add error messages to validation

--- gumpu/src/components/ui/SmartInput/index.js
+++ gumpu/src/components/ui/SmartInput/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Input from '../Input';
 
-function SmartInput({ className, label, valid, dirty, ...props }) {
-  const variant = valid === undefined || !dirty ? 'default' : (valid ? 'valid' : 'invalid');
+function SmartInput({ className, label, valid, validatable, dirty, ...props }) {
+  const variant = !validatable || !dirty ? 'default' : (valid === true ? 'valid' : 'invalid');
 
   return (
-    <>
-      <label className={`${className} column txt-input-label ${variant}`}>
+    <div className={`${className} relative`}>
+      <label className={`column w100 txt-input-label ${variant}`}>
         { label }
         <Input className="mt-8" variant={variant} {...props} />
       </label>
-    </>
+      { (variant === 'invalid') && <span className="absolute txt-tiny txt-red w100 txt-right pt-4">{ valid }</span> }
+    </div>
   );
 }
 
@@ -20,7 +21,8 @@ SmartInput.defaultProps = {
 }
 
 SmartInput.propTypes = {
-  valid: PropTypes.bool,
+  valid: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  validatable: PropTypes.bool,
   dirty: PropTypes.bool,
   className: PropTypes.string
 }
