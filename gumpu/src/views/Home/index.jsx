fbc1674831b98a1321866ec5369f12eb6a40ec51
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-23 00:42:50 -0300

    Fix text description

--- gumpu/src/views/Home/index.jsx
+++ gumpu/src/views/Home/index.jsx
@@ -10,6 +10,7 @@ function Home() {
       <div className="w100 row space-between pl-64 pr-64 mb-96">
         <h2 className={`${styles.registerMessage} txt-xlarge mt-32`}>
           <span className="txt-gray3">New to Gumpu?</span>
+          <br />
           <span className="txt-gray2">Create an account</span>
           <br />
           <span className="txt-green">now!</span>
