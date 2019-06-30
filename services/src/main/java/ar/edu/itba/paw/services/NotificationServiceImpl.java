98af678cedb73623799a3b34f2be513bd72e7313
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-30 04:42:34 -0300

    Add notification service implementation

---
+++ services/src/main/java/ar/edu/itba/paw/services/NotificationServiceImpl.java
@@ -0,0 +1,51 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.interfaces.dao.NotificationDao;
+import ar.edu.itba.paw.interfaces.service.NotificationService;
+import ar.edu.itba.paw.interfaces.service.UserService;
+import ar.edu.itba.paw.model.Notification;
+import ar.edu.itba.paw.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Primary
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+  @Autowired
+  private NotificationDao notificationDao;
+  @Autowired
+  private UserService userService;
+
+  @Override
+  @Transactional
+  public List<Notification> getLatest() {
+    final String loggedUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+    final Optional<User> loggedUser = userService.findByEmail(loggedUserEmail);
+
+    if (!loggedUser.isPresent()) {
+      throw new IllegalStateException("User is logged but it doesn't exist in DB");
+    }
+
+    List<Notification> notifications = notificationDao.getUnseen(loggedUser.get().getId());
+
+    return notifications;
+  }
+
+  @Override
+  @Transactional
+  public void markAllSeen() {
+    List<Notification> unseenNotifications = getLatest();
+    unseenNotifications.parallelStream().forEach(
+        notification -> {
+          notification.setSeen(true);
+          notificationDao.update(notification);
+        });
+  }
+}
