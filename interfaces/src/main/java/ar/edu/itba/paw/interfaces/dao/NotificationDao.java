d75bbf4744fe75838e8c56bed14e9d3b1f836faa
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-30 03:48:31 -0300

    Add methods to retrieve notifications to dao

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/dao/NotificationDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/dao/NotificationDao.java
@@ -1,9 +1,8 @@
 package ar.edu.itba.paw.interfaces.dao;
 
-import ar.edu.itba.paw.model.Notification;
-import ar.edu.itba.paw.model.NotificationType;
-import ar.edu.itba.paw.model.Order;
-import ar.edu.itba.paw.model.Publication;
+import ar.edu.itba.paw.model.*;
+
+import java.util.List;
 
 public interface NotificationDao {
   /**
@@ -13,7 +12,22 @@ public interface NotificationDao {
    * @param relatedOrder
    * @return The created notification
    */
-  public Notification create(final NotificationType type, final Publication relatedPublication, final Order relatedOrder);
+  public Notification create(final User user, final NotificationType type, final Publication relatedPublication, final Order relatedOrder);
+
+  /**
+   * Retrieves unseen notifications of user
+   * @param userId
+   * @return the list of notifications
+   */
+  public List<Notification> getUnseen(final Long userId);
+
+  /**
+   * Retrieves the latest notification for user
+   * @param userId
+   * @param limit
+   * @return the list of notifications
+   */
+  public List<Notification> getLatest(final Long userId, final Integer limit);
 
   /**
    * Updated the passed notification
