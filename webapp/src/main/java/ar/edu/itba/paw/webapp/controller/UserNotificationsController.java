91126b50ed29aaa8e68f5744a18efc87d179d2f0
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-06-30 22:51:45 -0300

    Add endpoint to mark all notifications as seen

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserNotificationsController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserNotificationsController.java
@@ -6,10 +6,7 @@ import ar.edu.itba.paw.webapp.dto.NotificationListDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -33,6 +30,17 @@ public class UserNotificationsController {
       return Response.ok(new NotificationListDTO(notifications)).build();
     } catch(IllegalStateException e) {
       return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
+    }
+  }
+
+  @DELETE
+  public Response markSeen() {
+    try {
+      notificationService.markAllSeen();
+
+      return Response.status(Response.Status.ACCEPTED).build();
+    } catch(IllegalStateException e) {
+      return Response.status(Response.Status.CONFLICT).build();
     }
   }
 
