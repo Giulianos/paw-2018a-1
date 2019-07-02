a8a2931a45d5cb11a1244aa5cb7b2ffbe404837c
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-02 03:39:24 -0300

    Add auth module to redux

---
+++ gumpu/src/redux/auth/actions.js
@@ -0,0 +1,8 @@
+const prefix = '@@AUTH';
+
+export default {
+  LOGIN: `${prefix}/login`,
+  LOGIN_OK: `${prefix}/login_ok`,
+  LOGIN_FAIL: `${prefix}/login_fail`,
+  LOGIN_RESET: `${prefix}/login_reset`
+};
