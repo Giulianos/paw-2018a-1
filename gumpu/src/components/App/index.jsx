23c738858eb6f5bf81144b2ec428489638073391
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-02 18:27:08 -0300

    Add search results view

--- gumpu/src/components/App/index.jsx
+++ gumpu/src/components/App/index.jsx
@@ -18,6 +18,7 @@ import Login from 'views/Login';
 
 import styles from './styles.module.scss';
 import useAuth from 'hooks/useAuth';
+import Search from 'views/Search';
 
 const token = localStorageService.getToken();
 
@@ -41,6 +42,7 @@ function App() {
           <Switch>
             <AuthenticatedRoute path="/secondary" component={Secondary} />
             <Route path="/login" component={Login} />
+            <Route path="/search/:term" component={Search} />
             <Route path="/" component={Home} />
           </Switch>
         </div>
