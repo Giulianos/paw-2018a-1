86c26fc208e5ce043934be03a758694b12ad7d41
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-02 03:39:57 -0300

    Add login view

---
+++ gumpu/src/views/Login/components/LoginForm/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import useFormInput from 'hooks/useFormInput';
+
+import emailValidator from 'validators/email';
+
+import {
+  login as loginAction,
+  resetLogin as resetLoginAction,
+} from 'redux/auth/actionCreators';
+
+import LoginFormLayout from './layout';
+
+function LoginForm({
+  login, loading, error, success, resetLogin,
+}) {
+  const form = {
+    email: useFormInput('', emailValidator),
+    password: useFormInput(''),
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    login({
+      email: form.email.value,
+      password: form.password.value,
+    });
+  };
+
+  return (
+    <LoginFormLayout
+      {...form}
+      handleSubmit={handleSubmit}
+      loading={loading}
+      error={error}
+      success={success}
+      resetRequest={resetLogin}
+    />
+  );
+}
+
+LoginForm.propTypes = {
+  resetLogin: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+};
+
+const mapStateToPros = state => ({
+  success: state.auth.login.success,
+  loading: state.auth.login.loading,
+  error: state.auth.login.error,
+});
+
+const mapDispatchToProps = dispatch => ({
+  login: credentials => dispatch(loginAction(credentials)),
+  resetLogin: () => dispatch(resetLoginAction()),
+});
+
+export default connect(
+  mapStateToPros,
+  mapDispatchToProps,
+)(LoginForm);
---
+++ gumpu/src/views/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
+function Login() {
+  return (
+    <div className="view-container column center-alt">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
