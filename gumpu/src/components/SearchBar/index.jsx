3d1cdf747ea8953d93047dced1a156f7a50f3ea3
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-02 18:37:48 -0300

    Add event handler to search box

--- gumpu/src/components/SearchBar/index.jsx
+++ gumpu/src/components/SearchBar/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import styles from './styles.module.scss';
-import Input from 'components/ui/Input';
-import Button from 'components/ui/Button';
+import useFormInput from 'hooks/useFormInput';
+import history from 'router/history';
+
+import SearchBarLayout from './layout';
 
 function SearchBar() {
-  return (
-    <div className={`column center-alt w100`}>
-      <h2 className="txt-large txt-gray3 mb-48">What are you looking for?</h2>
-      <div className={styles.searchBox}>
-        <Input id="search" placeholder="E.g. soda" className={`mr-8 ${styles.searchInput}`} />
-        <Button>SEARCH</Button>
-      </div>
-    </div>
-  );
+  const form = {
+    term: useFormInput('')
+  }
+
+  const search = e => {
+    e.preventDefault();
+    if(form.term.value !== '') {
+      history.push(`/search/${form.term.value}`);
+    }
+  }
+
+  return <SearchBarLayout handleSubmit={search} {...form} />
 }
 
 export default SearchBar;
