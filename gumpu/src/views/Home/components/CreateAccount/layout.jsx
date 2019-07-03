3a9b9f7160dbf941ecc98800133c037c87151190
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 20:12:06 -0300

    Add translation for create account labels

--- gumpu/src/views/Home/components/CreateAccount/layout.jsx
+++ gumpu/src/views/Home/components/CreateAccount/layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 
 import SmartInput from 'components/ui/SmartInput';
@@ -9,10 +9,12 @@ import Loader from 'components/ui/Loader';
 import styles from './styles.module.scss';
 import Failure from './components/Failure';
 import Success from './components/Success';
+import { useTranslation } from 'react-i18next';
 
 function CreateAccountLayout({
   name, email, password, handleSubmit, resetRequest, loading, error, success,
 }) {
+  const { t } = useTranslation();
   return (
     <CardContainer className={`${styles.cardContainer} column center-alt center`}>
       { loading && <Loader /> }
@@ -20,10 +22,10 @@ function CreateAccountLayout({
       { success && <Success /> }
       { (!loading && !error && !success) && (
         <form className="w100 column center-alt center" onSubmit={handleSubmit}>
-          <SmartInput id="signup-name" label="Name" className="mb-24 w100" {...name} />
-          <SmartInput id="signup-email" label="Email" className="mb-24 w100" {...email} />
-          <SmartInput id="signup-pass" label="New password" className="mb-32 w100" {...password} type="password" />
-          <Button type="submit" className="pl-32 pr-32">CREATE ACCOUNT</Button>
+          <SmartInput id="signup-name" label={t('home.register_form.name')} className="mb-24 w100" {...name} />
+          <SmartInput id="signup-email" label={t('home.register_form.email')} className="mb-24 w100" {...email} />
+          <SmartInput id="signup-pass" label={t('home.register_form.password')} className="mb-32 w100" {...password} type="password" />
+          <Button type="submit" className="pl-32 pr-32">{t('home.register_form.action')}</Button>
         </form>
       ) }
     </CardContainer>
@@ -41,4 +43,4 @@ CreateAccountLayout.propTypes = {
   success: PropTypes.bool,
 };
 
-export default CreateAccountLayout;
+export default props =><Suspense fallback={<div />}><CreateAccountLayout {...props} /></Suspense>;
