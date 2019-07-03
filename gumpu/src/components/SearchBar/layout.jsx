c0aa8525cbac3402b874d9fa91f7d630a5f799e3
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 19:30:30 -0300

    Add translations for SearchBar and Register now message

--- gumpu/src/components/SearchBar/layout.jsx
+++ gumpu/src/components/SearchBar/layout.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
 import Input from 'components/ui/Input';
 import Button from 'components/ui/Button';
 import styles from './styles.module.scss';
 
 function SearchBarLayout({ handleSubmit, term }) {
+  const { t } = useTranslation();
+
   return (
     <div className="column center-alt w100">
-      <h2 className="txt-large txt-gray3 mb-48">What are you looking for?</h2>
+      <h2 className="txt-large txt-gray3 mb-48">{t('search_bar.question')}</h2>
       <form onSubmit={handleSubmit} className={styles.searchBox}>
-        <Input {...term} id="search" placeholder="E.g. soda" className={`mr-8 ${styles.searchInput}`} />
-        <Button type="submit">SEARCH</Button>
+        <Input {...term} id="search" placeholder={t('search_bar.placeholder')} className={`mr-8 ${styles.searchInput}`} />
+        <Button type="submit">{t('search_bar.action')}</Button>
       </form>
     </div>
   );
@@ -21,4 +24,8 @@ SearchBarLayout.propTyeps = {
   handleSubmit: PropTypes.func.isRequired,
 };
 
-export default SearchBarLayout;
+function Loader() {
+  return <div />;
+}
+
+export default props => <Suspense fallback={<Loader />}><SearchBarLayout {...props} /></Suspense>;
--- gumpu/src/views/Home/index.jsx
+++ gumpu/src/views/Home/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 
 import SearchBar from 'components/SearchBar';
 import ProductCarrousel from 'components/ProductCarrousel';
@@ -6,6 +7,7 @@ import styles from './styles.module.scss';
 import CreateAccount from './components/CreateAccount';
 
 function Home() {
+  useTranslation();
   return (
     <div className="view-container column center-alt">
       <div className="w100 row mb-96">
@@ -16,11 +18,13 @@ function Home() {
       </div>
       <div className="w100 row space-between pl-64 pr-64">
         <h2 className={`${styles.registerMessage} txt-xlarge mt-32`}>
-          <span className="txt-gray3">New to Gumpu?</span>
-          <br />
-          <span className="txt-gray2">Create an account</span>
-          <br />
-          <span className="txt-green">now!</span>
+          <Trans i18nKey="home.register_now">
+            <span className="txt-gray3">New to Gumpu?</span>
+            <br />
+            <span className="txt-gray2">Create an account</span>
+            <br />
+            <span className="txt-green">now!</span>
+          </Trans>
         </h2>
         <CreateAccount />
       </div>
@@ -28,4 +32,8 @@ function Home() {
   );
 }
 
-export default Home;
+function Loader() {
+  return <div />;
+}
+
+export default () => <Suspense fallback={<Loader />}><Home /></Suspense>;
