e77136be8c0e1c5e8d0954c1ab54247bc336e614
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 19:49:22 -0300

    Fix search bar input

--- gumpu/src/components/SearchBar/layout.jsx
+++ gumpu/src/components/SearchBar/layout.jsx
@@ -9,11 +9,13 @@ import styles from './styles.module.scss';
 function SearchBarLayout({ handleSubmit, term }) {
   const { t } = useTranslation();
 
+  const { valid, validatable, dirty, ...termProps } = term;
+
   return (
     <div className="column center-alt w100">
       <h2 className="txt-large txt-gray3 mb-48">{t('search_bar.question')}</h2>
       <form onSubmit={handleSubmit} className={styles.searchBox}>
-        <Input {...term} id="search" placeholder={t('search_bar.placeholder')} className={`mr-8 ${styles.searchInput}`} />
+        <Input {...termProps} id="search" placeholder={t('search_bar.placeholder')} className={`mr-8 ${styles.searchInput}`} />
         <Button type="submit">{t('search_bar.action')}</Button>
       </form>
     </div>
