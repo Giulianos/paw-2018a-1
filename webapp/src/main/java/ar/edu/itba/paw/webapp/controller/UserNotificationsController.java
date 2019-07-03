73b964eea21e55622693c7c9ff888ace47c4fb56
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 03:25:57 -0300

    Change notifications endpoint

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserNotificationsController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserNotificationsController.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/user/{userId}/notifications")
+@Path("/users/{userId}/notifications")
 @Controller
 public class UserNotificationsController {
 
