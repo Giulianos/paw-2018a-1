57d869628cbde55c4e150b79d867c36f07b6327d
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 20:25:27 -0300

    Add translations to home

--- gumpu/src/views/Home/index.jsx
+++ gumpu/src/views/Home/index.jsx
@@ -7,14 +7,14 @@ import styles from './styles.module.scss';
 import CreateAccount from './components/CreateAccount';
 
 function Home() {
-  useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="view-container column center-alt">
       <div className="w100 row mb-96">
         <SearchBar />
       </div>
       <div className="w100 row pl-64 pr-64 mb-96">
-        <ProductCarrousel />
+        <ProductCarrousel title={t('home.latest_products')} />
       </div>
       <div className="w100 row space-between pl-64 pr-64">
         <h2 className={`${styles.registerMessage} txt-xlarge mt-32`}>
