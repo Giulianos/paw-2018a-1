a502eec427096a1824d160a2ab2c343a751eff59
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 20:12:20 -0300

    Add translation to login form

--- gumpu/src/views/Login/components/LoginForm/layout.jsx
+++ gumpu/src/views/Login/components/LoginForm/layout.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 
 import SmartInput from 'components/ui/SmartInput';
 import CardContainer from 'components/ui/CardContainer';
 import Button from 'components/ui/Button';
-
 import Loader from 'components/ui/Loader';
+
 import styles from './styles.module.scss';
 import Failure from './components/Failure';
 import Success from './components/Success';
+import { useTranslation } from 'react-i18next';
 
 function LoginFormLayout({
-  email, password, handleSubmit, resetRequest, loading, error, success,
+  email, password, handleSubmit, resetRequest, loading, error, success
 }) {
+  const { t } = useTranslation();
+
   return (
     <CardContainer className={`${styles.cardContainer} column center-alt center`}>
       { loading && <Loader /> }
       { error && <Failure handleRetry={resetRequest} /> }
       { success && <Success /> }
       { (!loading && !error && !success) && (
-        <form className="w100 column center-alt center" onSubmit={handleSubmit}>
-          <SmartInput id="email" label="Email" className="mb-24 w100" {...email} />
-          <SmartInput id="password" label="Password" className="mb-32 w100" {...password} type="password" />
-          <Button type="submit" className="pl-32 pr-32">LOGIN</Button>
-        </form>
+        <>
+          <h1 className="txt-large mb-64 txt-gray3">{t('login.title')}</h1>
+          <form className="w100 column center-alt center" onSubmit={handleSubmit}>
+            <SmartInput id="email" label={t('login.email')} className="mb-24 w100" {...email} />
+            <SmartInput id="password" label={t('login.password')} className="mb-32 w100" {...password} type="password" />
+            <Button type="submit" className="pl-32 pr-32">{t('login.action')}</Button>
+          </form>
+        </>
       ) }
     </CardContainer>
   );
@@ -39,4 +45,4 @@ LoginFormLayout.propTypes = {
   success: PropTypes.bool,
 };
 
-export default LoginFormLayout;
+export default props => <Suspense fallback={<div />}><LoginFormLayout {...props} /></Suspense>;
