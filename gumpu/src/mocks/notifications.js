2439ada77ba1beb57d054b4756ad2a676fc0facc
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-03 05:01:10 -0300

    Add mock notifications

--- gumpu/src/mocks/notifications.js
+++ gumpu/src/mocks/notifications.js
@@ -31,5 +31,79 @@ export const orderPurchased = {
     publicationId: 1,
     publicationUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/publications/1'
   },
-  type: 'NEW_MESSAGES'
+  relatedPublication: {
+    availableQuantity: 0,
+    description: 'CocaCola',
+    detailedDescription: 'Gaseosa CocaCola, botella de 1.5L por unidad',
+    id: 1,
+    imagesUrls: [],
+    quantity: 10,
+    status: 'PURCHASED',
+    supervisorId: 1,
+    supervisorUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/1',
+    tags: [],
+    unitPrice: 12
+  },
+  type: 'ORDER_PURCHASED'
+};
+
+export const orderFulfilledSupervisor = {
+  relatedPublication: {
+    availableQuantity: 0,
+    description: 'CocaCola',
+    detailedDescription: 'Gaseosa CocaCola, botella de 1.5L por unidad',
+    id: 1,
+    imagesUrls: [],
+    quantity: 10,
+    status: 'FULFILLED',
+    supervisorId: 1,
+    supervisorUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/1',
+    tags: [],
+    unitPrice: 12
+  },
+  type: 'PUBLICATION_FULFILLED'
+};
+
+export const orderFulfilledOrderer = {
+  relatedOrder: {
+    ordererId: 3,
+    ordererUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/3',
+    publicationId: 1,
+    publicationUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/publications/1'
+  },
+  relatedPublication: {
+    availableQuantity: 0,
+    description: 'CocaCola',
+    detailedDescription: 'Gaseosa CocaCola, botella de 1.5L por unidad',
+    id: 1,
+    imagesUrls: [],
+    quantity: 10,
+    status: 'FULFILLED',
+    supervisorId: 1,
+    supervisorUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/1',
+    tags: [],
+    unitPrice: 12
+  },
+  type: 'PUBLICATION_FULFILLED'
+};
+
+export const supervisorLeftPublication = {
+  relatedOrder: {
+    ordererId: 3,
+    ordererUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/3',
+    publicationId: 1,
+    publicationUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/publications/1'
+  },
+  relatedPublication: {
+    availableQuantity: 2,
+    description: 'CocaCola',
+    detailedDescription: 'Gaseosa CocaCola, botella de 1.5L por unidad',
+    id: 1,
+    imagesUrls: [],
+    quantity: 10,
+    status: 'ORPHAN',
+    tags: [],
+    unitPrice: 12
+  },
+  type: 'PUBLICATION_ORPHAN'
 };
