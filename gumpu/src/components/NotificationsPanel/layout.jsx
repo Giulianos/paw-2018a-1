213a28c23ff400a4bf8096c3ee06042f771f67df
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-04 21:26:00 -0300

    Add integration to notifications panel

---
+++ gumpu/src/components/NotificationsPanel/layout.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import CardContainer from 'components/ui/CardContainer';
+import {
+  supervisorLeftPublication,
+  orderFulfilledOrderer,
+  orderFulfilledSupervisor,
+  orderPurchased,
+  newMessage,
+} from 'mocks/notifications';
+
+import NotificationText from './NotificationText';
+import styles from './styles.module.scss';
+import { getNotificationLink } from './helpers';
+
+const sampleImage = 'https://www.officedepot.com.mx/medias/84266.jpg-515ftw?context=bWFzdGVyfHJvb3R8NzUxNDh8aW1hZ2UvanBlZ3xoOTIvaDA3Lzk1NzAyMzY5MjM5MzQuanBnfDAyMzQxNTRlMTdjMzczYmRhZThlN2I0MDNhOGIzZjgyY2RlMWRlZjY4ZjVkZTNjYzQ1MzM3YWZkMzhlY2YyZDc';
+
+function NotificationsPanelLayout({ data, loading, success, error }) {
+  const { t } = useTranslation();
+
+  return (
+    <CardContainer className={styles.container}>
+      <div className="pt-16 pb-16 pl-16 txt-bold txt-gray3">{t('header.notifications')}</div>
+      <ul>
+        { !success ? <li>Loading</li> : data.notifications.map(n => (
+        <Link key={n.id} to={getNotificationLink(n)}>
+          <li className={styles.notification}>
+            <img alt="" src={sampleImage} className={styles.image} />
+            <div className={styles.details}><NotificationText notification={n} /></div>
+          </li>
+        </Link>)) }
+      </ul>
+    </CardContainer>
+  );
+}
+
+export default NotificationsPanelLayout;
