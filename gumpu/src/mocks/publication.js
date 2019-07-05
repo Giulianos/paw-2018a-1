26710083628f71f82aace4803e45620e4ddb4b1d
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-05 01:46:41 -0300

    Add sample mocks

---
+++ gumpu/src/mocks/publication.js
@@ -0,0 +1,17 @@
+export default {
+  availableQuantity: 100,
+  description: 'Cuaderno Scribe azul tapa dura',
+  detailedDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed feugiat sapien. Phasellus sit amet massa tincidunt, sagittis erat eget, ornare augue. Praesent tortor nibh, posuere ac venenatis eu, interdum ac dolor. Sed nec orci blandit, tincidunt lacus eu, tristique libero. ',
+  id: 6,
+  imagesUrls: [],
+  quantity: 100,
+  status: 'IN_PROGRESS',
+  supervisorId: 3,
+  supervisorUrl: 'http://pawserver.it.itba.edu.ar/paw-2018a-1/api/users/3',
+  tags: [
+    'azul',
+    'cuaderno',
+    'scribe'
+  ],
+  unitPrice: 120
+};
