4c605c49d820b19fe0bd84f13360d83f5d3f259e
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-05 02:14:55 -0300

    Add create account in login

--- gumpu/src/views/Login/components/LoginForm/layout.jsx
+++ gumpu/src/views/Login/components/LoginForm/layout.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import SmartInput from 'components/ui/SmartInput';
 import CardContainer from 'components/ui/CardContainer';
@@ -9,7 +10,7 @@ import Loader from 'components/ui/Loader';
 import styles from './styles.module.scss';
 import Failure from './components/Failure';
 import Success from './components/Success';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 function LoginFormLayout({
   email, password, handleSubmit, resetRequest, loading, error, success
@@ -24,11 +25,12 @@ function LoginFormLayout({
       { (!loading && !error && !success) && (
         <>
           <h1 className="txt-large mb-64 txt-gray3">{t('login.title')}</h1>
-          <form className="w100 column center-alt center" onSubmit={handleSubmit}>
+          <form className="w100 column center-alt center pb-48" onSubmit={handleSubmit}>
             <SmartInput id="email" label={t('login.email')} className="mb-24 w100" {...email} />
             <SmartInput id="password" label={t('login.password')} className="mb-32 w100" {...password} type="password" />
             <Button type="submit" className="pl-32 pr-32">{t('login.action')}</Button>
           </form>
+          <div><Trans i18nKey="login.register_hint">Don't have an account? <Link className="txt-blue" to="/create-account">Create one now!</Link></Trans></div>
         </>
       ) }
     </CardContainer>
