9d738cc8aa14aec8118568af5563493600cdaee3
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-08 11:37:45 -0300

    Add service to send message

--- gumpu/src/services/message.js
+++ gumpu/src/services/message.js
@@ -3,12 +3,14 @@ import api from 'api';
 export default {
   order: {
     list: (userId, orderId) => api.get(`/users/${userId}/orders/${orderId}/messages`),
+    send: (userId, orderId, message) => api.post(`/users/${userId}/orders/${orderId}/messages`, { message }),
     unseen: {
       list: (userId, orderId) => api.get(`/users/${userId}/orders/${orderId}/messages/unseen`)
     }
   },
   publication: {
     list: (publicationId, ordererId) => api.get(`publications/${publicationId}/orders/${ordererId}/messages`),
+    send: (publicationId, ordererId, message) => api.post(`publications/${publicationId}/orders/${ordererId}/messages`, { message }),
     unseen: {
       list: (publicationId, ordererId) => api.get(`publications/${publicationId}/orders/${ordererId}/messages/unseen`)
     }
