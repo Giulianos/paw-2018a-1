36e54493d6faacdb965d85860e6b0701847be8a2
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-09 11:12:51 -0300

    Add search view

--- gumpu/src/views/Search/index.jsx
+++ gumpu/src/views/Search/index.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import ProductCard from 'components/ui/ProductCard';
 
-function Search({ match }) {
+import styles from './styles.module.scss';
+import { useTranslation } from 'react-i18next';
+import Loader from 'components/ui/Loader';
+
+function SearchSuspense({ match }) {
   const searchTerm = match.params.term;
+  const { t } = useTranslation();
 
   return (
-    <div className="view-container">
-Search results for:
-      {searchTerm}
+    <div className="view-container column center-alt">
+      <div className={styles.searchResults}>
+        <h1 className="txt-large mb-24 txt-gray3 w100">{t('search.title', { term: searchTerm })}</h1>
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+        <ProductCard className={styles.product} />
+      </div>
     </div>
   );
+}
+
+function Search(props) {
+  return (
+    <Suspense fallback={<Loader />}>
+      <SearchSuspense {...props} />
+    </Suspense>
+  )
 }
 
 export default Search;
