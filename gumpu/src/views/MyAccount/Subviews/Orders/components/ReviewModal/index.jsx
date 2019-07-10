22b6624101b004cdda48e1bf02e568473b9020ee
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 00:38:47 -0300

    Add form validation

--- gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/index.jsx
+++ gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReviewModalLayout from './layout';
 import useFormInput from 'hooks/useFormInput';
+import quantityMax from 'validators/quantityMax';
 
 function ReviewModal() {
 
   const reviewForm = {
     comment: useFormInput(''),
-    rating: useFormInput('')
+    rating: useFormInput('', quantityMax(5, ''))
   }
 
   const handleReview = e => {
     e.preventDefault();
-    console.log(`Comment: ${reviewForm.comment.value}`)
-    console.log(`Rating: ${reviewForm.rating.value}`)
+    if(reviewForm.rating.valid === true) {
+      console.log(`Comment: ${reviewForm.comment.value}`)
+      console.log(`Rating: ${reviewForm.rating.value}`)
+    }
   }
 
   return <ReviewModalLayout reviewForm={reviewForm} onReview={handleReview} />;
