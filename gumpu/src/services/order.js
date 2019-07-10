05ce604083491b9c522861360c1ec6933c15ac44
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 02:08:42 -0300

    Add confirm/review order to service

--- gumpu/src/services/order.js
+++ gumpu/src/services/order.js
@@ -3,5 +3,7 @@ import api from 'api';
 const ENDPOINT = userId => `users/${userId}/orders`;
 
 export default {
-  list: (userId, page, pageSize) => api.get(ENDPOINT(userId), { page, pageSize })
+  list: (userId, page, pageSize) => api.get(ENDPOINT(userId), { page, pageSize }),
+  rate: (userId, orderId, rating, comment) => api.post(`${ENDPOINT(userId)}/${orderId}/reviews`, { rating, comment }),
+  confirm: (userId, orderId) => api.post(`${ENDPOINT(userId)}/${orderId}/confirmation`)
 };
