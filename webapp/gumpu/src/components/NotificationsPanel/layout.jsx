0a221e4ea83ea1062401c51e72538c482ae4b960
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 13:44:54 -0300

    Add clear notifications

--- webapp/gumpu/src/components/NotificationsPanel/layout.jsx
+++ webapp/gumpu/src/components/NotificationsPanel/layout.jsx
@@ -8,12 +8,15 @@ import NotificationText from './NotificationText';
 import styles from './styles.module.scss';
 import { getNotificationLink } from './helpers';
 
-function NotificationsPanelLayout({ data, success }) {
+function NotificationsPanelLayout({ data, success, onClear }) {
   const { t } = useTranslation();
 
   return (
     <CardContainer className={styles.container}>
-      <div className="pt-16 pb-16 pl-16 txt-bold txt-gray3">{t('header.notifications')}</div>
+      <div className="pt-16 pb-16 pl-16 pr-16 txt-bold txt-gray3 row space-between">
+        {t('header.notifications')}
+        { success && data.notifications.length > 0 && <button onClick={onClear} className="txt-red">{t('notifications.clear')}</button> }
+      </div>
       <ul>
         { !success ?
         <li className={styles.skeletonNotification}><div /><div /></li> :
--- webapp/gumpu/src/redux/notifications/actions.js
+++ webapp/gumpu/src/redux/notifications/actions.js
@@ -5,4 +5,9 @@ export default {
   RETRIEVE_OK: `${prefix}/retrieve_ok`,
   RETRIEVE_FAIL: `${prefix}/retrieve_fail`,
   RETRIEVE_RESET: `${prefix}/retrieve_reset`,
+
+  CLEAR: `${prefix}/clear`,
+  CLEAR_OK: `${prefix}/clear_ok`,
+  CLEAR_FAIL: `${prefix}/clear_fail`,
+  CLEAR_RESET: `${prefix}/clear_reset`,
 };
