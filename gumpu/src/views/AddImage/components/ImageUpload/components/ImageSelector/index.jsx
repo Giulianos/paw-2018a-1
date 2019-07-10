d7f3eef217dc1d7eef55c1cbd404f986fc88a2b8
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 05:46:20 -0300

    Move responsabilities to smart component

--- gumpu/src/views/AddImage/components/ImageUpload/components/ImageSelector/index.jsx
+++ gumpu/src/views/AddImage/components/ImageUpload/components/ImageSelector/index.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
+import {fitImage} from './utils';
 import ImageSelectorLayout from './layout';
 
 function ImageSelector() {
+  const canvasRef = useRef(null);
+  const imageChangedHandler = e => {
+    e.preventDefault();
+    const context = canvasRef.current.getContext('2d');
+    const image = new Image();
+    const reader = new FileReader();
+    reader.onload = () => {
+      image.onload = () => {
+        fitImage(context, image, 208, 208);
+      };
+      image.src = reader.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
 
-  return <ImageSelectorLayout />;
+  return <ImageSelectorLayout onImageChange={imageChangedHandler} canvasRef={canvasRef} />;
 }
 
 export default ImageSelector;
--- gumpu/src/views/AddImage/components/ImageUpload/components/ImageSelector/layout.jsx
+++ gumpu/src/views/AddImage/components/ImageUpload/components/ImageSelector/layout.jsx
@@ -1,28 +1,11 @@
-import React, { useRef } from 'react';
-
-import {fitImage} from './utils';
+import React from 'react';
 
 import styles from './styles.module.scss';
 
-function ImageSelectorLayout() {
-  const canvasRef = useRef(null);
-  const imageChanged = e => {
-    e.preventDefault();
-    const context = canvasRef.current.getContext('2d');
-    const image = new Image();
-    const reader = new FileReader();
-    reader.onload = () => {
-      image.onload = () => {
-        fitImage(context, image, 208, 208);
-      };
-      image.src = reader.result;
-    }
-    reader.readAsDataURL(e.target.files[0]);
-  }
-
+function ImageSelectorLayout({ onImageChange, canvasRef }) {
   return (
     <label>
-      <input onChange={imageChanged} className={styles.hiddenInput} type="file" id="image" />
+      <input onChange={onImageChange} className={styles.hiddenInput} type="file" id="image" />
       <canvas width="208" height="208" className={styles.container} ref={canvasRef} />
     </label>
   );
