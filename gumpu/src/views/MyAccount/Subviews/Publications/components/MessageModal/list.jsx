97e9ae9c83f34fc9d32da3a4be3cadf985e7751a
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 07:33:32 -0300

    Add missing translations

--- gumpu/src/views/MyAccount/Subviews/Publications/components/MessageModal/list.jsx
+++ gumpu/src/views/MyAccount/Subviews/Publications/components/MessageModal/list.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import CardContainer from 'components/ui/CardContainer';
 
 import styles from './styles.module.scss';
 import useAuth from 'hooks/useAuth';
+import Loader from 'components/ui/Loader';
+import { useTranslation } from 'react-i18next';
 
 
-function OrdersList({ className, orders }) {
+function OrdersListSuspense({ className, orders }) {
+  const { t } = useTranslation();
+
   if(orders.loading) {
-    return <div>Cargando...</div>;
+    return <Loader />;
   }
 
   return (
@@ -16,11 +20,22 @@ function OrdersList({ className, orders }) {
       <h1 className="txt-medium txt-black pt-24 pl-16 mb-32">Suscriptores</h1>
       <ul>
         { orders.orders.filter(o => (o.orderer.id !== o.publication.supervisorId)).map(o => (
-          <Link to={`/my-account/publications/${o.publication.id}/messages/${o.orderer.id}`}><li key={o.orderer.id} className={styles.order}><span className="txt-black txt-normal mb-4">{o.orderer.name}</span><span className="txt-gray2 txt-small">Ordeno {o.quantity}</span></li></Link>
+          <Link to={`/my-account/publications/${o.publication.id}/messages/${o.orderer.id}`}>
+            <li key={o.orderer.id} className={styles.order}>
+              <span className="txt-black txt-normal mb-4">{o.orderer.name}</span>
+              <span className="txt-gray2 txt-small">{t('my_account.publications.messages.ordered', {quantity: o.quantity})}</span>
+            </li>
+          </Link>
         ))
         }
       </ul>
     </CardContainer>
+  );
+}
+
+function OrdersList(props) {
+  return (
+    <OrdersListSuspense {...props} />
   );
 }
 
