399d5a7ae10cb85466d8ac65ac166fbcc103b35b
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-07-10 00:32:38 -0300

    Add form to review

--- gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/index.jsx
+++ gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReviewModalLayout from './layout';
+import useFormInput from 'hooks/useFormInput';
 
 function ReviewModal() {
-  return <ReviewModalLayout />;
+
+  const reviewForm = {
+    comment: useFormInput(''),
+    rating: useFormInput('')
+  }
+
+  const handleReview = e => {
+    e.preventDefault();
+    console.log(`Comment: ${reviewForm.comment.value}`)
+    console.log(`Rating: ${reviewForm.rating.value}`)
+  }
+
+  return <ReviewModalLayout reviewForm={reviewForm} onReview={handleReview} />;
 }
 
 export default ReviewModal;
--- gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/layout.jsx
+++ gumpu/src/views/MyAccount/Subviews/Orders/components/ReviewModal/layout.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import Loader from 'components/ui/Loader';
 import { useTranslation } from 'react-i18next';
 import Modal from 'components/ui/Modal';
@@ -9,24 +9,28 @@ import SmartInput from 'components/ui/SmartInput';
 import CardContainer from 'components/ui/CardContainer';
 import Button from 'components/ui/Button';
 
-function ReviewModalLayoutSuspense() {
+function ReviewModalLayoutSuspense({onReview, reviewForm}) {
   const { t } = useTranslation();
+
   return (
     <Modal>
       <div class={styles.container}>
         <h1 className="txt-xlarge mb-32">{t('my_account.orders.confirm_modal.title')}</h1>
         <CardContainer className={styles.reviewContainer}>
-          <span className="txt-medium20 txt-gray2">{t('my_account.orders.confirm_modal.leave_review.hint')}</span>
-          <SmartTextArea className="mt-24 mb-16" label={t('my_account.orders.confirm_modal.leave_review.comment_label')} />
-          <div class="row flex-end-alt mb-24">
-            <SmartInput label={t('my_account.orders.confirm_modal.leave_review.rating_label')} className={styles.shortInput} placeholder="5"/>
-            <span className={`${styles.ratingDescription} txt-small txt-gray2`}>{t('my_account.orders.confirm_modal.leave_review.rating_description')}</span>
-          </div>
-          <div className="row w100 flex-end">
-            <Button variant="green">{t('my_account.orders.confirm_modal.leave_review.action')}</Button>
-          </div>
+          <form onSubmit={onReview}>
+            <span className="txt-medium20 txt-gray2">{t('my_account.orders.confirm_modal.leave_review.hint')}</span>
+            <SmartTextArea {...reviewForm.comment} className="mt-24 mb-16" label={t('my_account.orders.confirm_modal.leave_review.comment_label')} />
+            <div class="row flex-end-alt mb-24">
+              <SmartInput {...reviewForm.rating} label={t('my_account.orders.confirm_modal.leave_review.rating_label')} className={styles.shortInput} placeholder="5"/>
+              <span className={`${styles.ratingDescription} txt-small txt-gray2`}>{t('my_account.orders.confirm_modal.leave_review.rating_description')}</span>
+            </div>
+            <div className="row w100 flex-end">
+              <Button variant="green">{t('my_account.orders.confirm_modal.leave_review.action')}</Button>
+            </div>
+          </form>
         </CardContainer>
-        <div class="row w100 flex-end">
+        <div class="row w100 space-between center-alt">
+          <span className={`${styles.ratingDescription} txt-normal txt-gray3`}>{t('my_account.orders.confirm_modal.leave_review.no_thanks')}</span>
           <Button>{t('my_account.orders.confirm_modal.action')}</Button>
         </div>
       </div>
